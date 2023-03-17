02ca0499512070177800d9c2a2f1a9120f74dbdc
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-03-17 13:07:08 +0200

    Organizing

---
+++ Sortlib.h
@@ -0,0 +1,13 @@
+#ifndef SORTLIB_H
+#define SORTLIB_H
+
+#include<iostream>
+
+namespace Sortlib {
+    template<typename T>
+    void InsertionSort(T arr[]) {
+        return;
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "Sortlib.h"
+using namespace std;
+
+
+int main() {
+    int arr[3] = { 2,3,1 };
+    Sortlib::InsertionSort(arr);
+    cout << "test";
+}
